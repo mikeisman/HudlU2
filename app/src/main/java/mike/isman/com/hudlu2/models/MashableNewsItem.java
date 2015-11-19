80539069d3b62766731f94ff5261a3e2774ce347
Mike Isman <mikeisman@gmail.com>
2015-11-18 20:43:53 -0500

    Step 5-6: Add models package and MashableNewsItem

---
+++ app/src/main/java/mike/isman/com/hudlu2/models/MashableNewsItem.java
@@ -0,0 +1,11 @@
+package mike.isman.com.hudlu2.models;
+
+/**
+ * Created by mike on 11/18/15.
+ */
+public class MashableNewsItem {
+    public String title;
+    public String author;
+    public String link;
+    public String image;
+}
