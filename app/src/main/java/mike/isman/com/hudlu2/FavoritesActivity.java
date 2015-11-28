8eae67985f17e47c14c883de53589df6609e0005
Mike Isman <mikeisman@gmail.com>
2015-11-27 22:02:29 -0500

    Step 9: Create FavoritesActivity

---
+++ app/src/main/java/mike/isman/com/hudlu2/FavoritesActivity.java
@@ -0,0 +1,13 @@
+package mike.isman.com.hudlu2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FavoritesActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorites);
+    }
+}
